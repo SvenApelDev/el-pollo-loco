3fa992fd274bbe56bdb2413f6d4febc104dad1d2
Sven Apel <svap1201@gmail.com>
2026-09-01 15:13:11 +0200

    feat: load clouds via ImageHub and add wrap-around

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,22 +3,34 @@ class Cloud extends MovableObject {
 	height = 250;
 	width = 500;
 	speed = 0.2;
+
 	/**
 	 * Creates a cloud at a given x position and starts its movement.
 	 * @param {number} x - base x position
 	 */
 	constructor(x) {
 		super();
-		this.loadImage('img/5_background/layers/4_clouds/1.png');
+		this.loadImage(
+			ImageHub.CLOUDS[Math.floor(Math.random() * ImageHub.CLOUDS.length)],
+		);
 		this.x = x + Math.random() * 200;
 		this.animate();
 	}
+    
 	/**
 	 * Moves the cloud left continuously.
 	 */
 	animate() {
-		setInterval(() => {
-			this.moveLeft();
-		}, 1000 / 60);
+		IntervalHub.startInterval(() => this.moveCloud(), 1000 / 60);
+	}
+
+	/**
+	 * Moves the cloud left and resets it to the right when off-screen.
+	 */
+	moveCloud() {
+		this.moveLeft();
+		if (this.x + this.width < -720) {
+			this.x = 1440 + Math.random() * 500;
+		}
 	}
 }
---
+++ models/image-hub.class.js
@@ -0,0 +1,7 @@
+
+class ImageHub {
+    static CLOUDS = [
+        'img/5_background/layers/4_clouds/1.png',
+        'img/5_background/layers/4_clouds/2.png',
+    ];
+}
