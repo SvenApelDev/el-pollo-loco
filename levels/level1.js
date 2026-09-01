be6243985cdb4c4f56e9f54d5e301a68a864cbdd
Sven Apel <svap1201@gmail.com>
2026-09-01 09:35:00 +0200

    feat: render level backgrounds via World draw loop

---
+++ levels/level1.js
@@ -0,0 +1,10 @@
+
+const level1 = new Level(
+    [], // enemies - leer
+    [], // clouds - leer
+    [
+        new BackgroundObject(
+            'img/5_background/layers/air.png', 0
+        )
+    ]
+);
--- models/world.class.js
+++ models/world.class.js
@@ -2,7 +2,13 @@ class World {
 	canvas;
 	ctx;
 	keyboard;
+	level = level1;
 
+	/**
+	 * Initializes the canvas, input controllers, and starts the render loop.
+	 * @param {HTMLCanvasElement} canvas - The HTML canvas element for rendering.
+	 * @param {Keyboard} keyboard - The keyboard input controller.
+	 */
 	constructor(canvas, keyboard) {
 		this.ctx = canvas.getContext("2d");
 		this.canvas = canvas;
@@ -11,14 +17,29 @@ class World {
 		this.draw();
 	}
 
+	/**
+	 * Links game objects to the current world instance:
+	 */
 	setWorld() {
 		// character.world = this
 	}
 
+	/**
+	 * Clears the canvas and draws all game objects continuously.
+	 */
 	draw() {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.ctx.fillStyle = "white";
-		this.ctx.fillRect(50, 50, 100, 100); //testweise - wird später entfernt!!!
+		this.addObjectsToMap(this.level.backgroundObjects);
 		requestAnimationFrame(() => this.draw());
+	}
+
+	/**
+	 * Renders an array of game objects onto the canvas context.
+	 * @param {DrawableObject[]} objects - Array of objects with a draw method.
+	 */
+	addObjectsToMap(objects) {
+		objects.forEach((object) => {
+			object.draw(this.ctx);
+		});
 	}
 }
