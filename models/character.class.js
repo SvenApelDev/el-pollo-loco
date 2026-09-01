79f6f094126343a116c6d15bf3682b3d09573a85
Sven Apel <svap1201@gmail.com>
2026-09-01 16:42:21 +0200

    feat: add idle animation to Character

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,8 @@
-
 class Character extends MovableObject {
 	height = 280;
 	width = 150;
 	y = 150;
-    x = 100;
+	x = 100;
 	speed = 10;
 	world;
 
@@ -19,5 +18,16 @@ class Character extends MovableObject {
 		this.loadImages(ImageHub.PEPE.hurt);
 		this.loadImages(ImageHub.PEPE.dead);
 		this.loadImages(ImageHub.PEPE.longIdle);
+		this.animate();
+	}
+
+	/**
+	 * Starts the character's idle animation loop.
+	 */
+	animate() {
+		IntervalHub.startInterval(
+			() => this.playAnimation(ImageHub.PEPE.idle),
+			1000 / 6,
+		);
 	}
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,22 @@
 
 class MovableObject extends DrawableObject {
-    speed = 0.15;
+	speed = 0.15;
+    currentImage = 0;
+	/**
+	 * Moves the object to the left based on its current speed.
+	 */
+	moveLeft() {
+		this.x -= this.speed;
+	}
 
-/**
- * Moves the object to the left based on its current speed.
- */  
-    moveLeft() {
-        this.x -= this.speed;
-    }
+	/**
+	 * Cycles through an animation array by updating this.img each call.
+	 * @param {string[]} images - array of image paths to play
+	 */
+	playAnimation(images) {
+		let i = this.currentImage % images.length;
+		let path = images[i];
+		this.img = this.imageCache[path];
+		this.currentImage++;
+	}
 }
