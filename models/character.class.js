2ea172976af5018cbf3131c846afd9d8abd80d1f
Sven Apel <svap1201@gmail.com>
2026-09-02 09:47:31 +0200

    docs: fix animate JSDoc and formating

--- models/character.class.js
+++ models/character.class.js
@@ -22,25 +22,25 @@ class Character extends MovableObject {
 	}
 
 	/**
-	 * Starts the character's idle animation loop.
+	 * Starts the character's movement and animation loops.
 	 */
 	animate() {
-        IntervalHub.startInterval(() => this.checkMovement(), 1000 / 60);
+		IntervalHub.startInterval(() => this.checkMovement(), 1000 / 60);
 		IntervalHub.startInterval(
 			() => this.playAnimation(ImageHub.PEPE.idle),
 			1000 / 6,
 		);
 	}
 
-    /**
-     * Reads keyboard input and moves the character accordingly.
-     */
-    checkMovement() {
-        if (this.world.keyboard.RIGHT) {
-            this.moveRight();
-        }
-        if (this.world.keyboard.LEFT) {
-            this.moveLeft();
-        }
-    }
+	/**
+	 * Reads keyboard input and moves the character accordingly.
+	 */
+	checkMovement() {
+		if (this.world.keyboard.RIGHT) {
+			this.moveRight();
+		}
+		if (this.world.keyboard.LEFT) {
+			this.moveLeft();
+		}
+	}
 }
