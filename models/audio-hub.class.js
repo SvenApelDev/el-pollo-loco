81bdfea107cc69e6fc5455e690c1cdc689c349b1
Sven Apel <svap1201@gmail.com>
2026-09-10 10:14:46 +0200

    feat: build AudioHub with grouped sounds, mute state, volume control

--- models/audio-hub.class.js
+++ models/audio-hub.class.js
@@ -1,23 +1,47 @@
 class MyAudio {
-    file;
-    isLoaded;
+	file;
+	isLoaded;
 
-    constructor(_file) {
-        this.file = new Audio(_file);
-    }
+	constructor(_file) {
+		this.file = new Audio(_file);
+	}
 }
 
 class AudioHub {
-    static WALKING = new MyAudio("sounds/character/characterRun.mp3");
-    static JUMPING = new MyAudio("sounds/character/characterJump.wav");
-    static HURT = new MyAudio("sounds/character/characterDamage.mp3");
-    static SNORING = new MyAudio("sounds/character/characterSnoring.mp3");
-    static CHICKEN_DEAD = new MyAudio("sounds/character/characterDead.wav");
-    static COIN = new MyAudio("sounds/collectibles/collectSound.wav");
-    static BOTTLE = new MyAudio("sounds/collectibles/bottleCollectSound.wav");
-    static THROW = new MyAudio("sounds/throwable/bottleBreak.mp3");
-    static ENDBOSS_HURT = new MyAudio("sounds/endboss/endbossApproach.wav");
-    static GAME_MUSIC = new MyAudio("sounds/game/gameStart.mp3");
-    static WIN = new MyAudio("");
-    static LOSE = new MyAudio("");
+    static PEPE = {
+        walk: new MyAudio("sounds/character/characterRun.mp3"),
+        jump: new MyAudio("sounds/character/characterJump.mp3"),
+        hurt: new MyAudio("sounds/character/characterDamage.mp3"),
+        snore: new MyAudio("sounds/character/characterSnoring.mp3"),
+        dead: new MyAudio("sounds/character/characterDead.wav"),
+    };
+
+    static CHICKEN = {
+        dead: new MyAudio("sounds/chicken/trampledSound.wav"),
+    };
+
+    static ENDBOSS = {
+        attack: new MyAudio("sounds/endboss/endbossAttack.wav"),
+        hurt: new MyAudio("sounds/endboss/endbossHurt.mp3"),
+        dead: new MyAudio("sounds/endboss/endbossDead.mp3"),
+    };
+
+    static COLLECT = {
+        coin: new MyAudio("sounds/collectibles/collectSound.wav"),
+        bottle: new MyAudio("sounds/collectibles/bottleCollectSound.wav"),
+    };
+
+    static BOTTLE = {
+        throw: new MyAudio("sounds/throwable/bottleThrowing.wav"),
+        break: new MyAudio("sounds/throwable/bottleBreak.mp3"),
+    };
+
+    static GAME = {
+        gamePlay: new MyAudio("sounds/game/playMusic.mp3"),
+        start: new MyAudio("sounds/game/startSound.mp3"),
+        end: new MyAudio("sounds/game/endMusic.mp3"),
+        win: new MyAudio("sounds/game/winMusic.mp3"),
+        lose: new MyAudio("sounds/game/loseMusic.mp3"),
+    };
+
 }
